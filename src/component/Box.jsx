d67972b641297535ce4568d275562192fd7d8ab3
goldstargreat <luckygoldsta@naver.com>
2025-12-15 21:10:00 +0900

    누가 이겼는지 승패를 가른다(사용자, 컴퓨터 서로 반전 시키기)

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import initialImage from "../assets/initialImage.jpg";
 
 const Box = (props) => {
+    let result;
+    if(
+        props.title === "Computer" &&
+        props.result !== "tie" &&
+        props.result !== ""
+    ) {
+        // 카드가 Computer의 카드인가? && 결과가 비겼는가? && props.result에 값이 있는가?
+        result = props.result ===  "win" ? "lose" : "win";
+    } else {
+        // 위의 경우가 아니라면 props 로 전달된 결과를 그대로 쓴다.
+        result = props.result;
+    } if (props.title === "Computer") {
+        console.log("computer", result);
+    }
+
     return (
         <div className="box">
             <h1>{props.title}</h1>
